7950dc9b80e756f07a6b86f01ab5bba5b7c65c39
zar3bski <zarebskidavid@gmail.com>
2025-07-07 16:15:31 +0200

    feat: biquad LPF

--- src/filters.rs
+++ src/filters.rs
@@ -1,4 +1,4 @@
-use std::ops::Mul;
+use std::{collections::VecDeque, ops::Mul};
 
 use wavers::{Samples, Wav};
 
@@ -43,26 +43,121 @@ fn single_pole_low_pass_filter(audio: &mut AudioMaterial, cutoff_frequency: u16)
     }
 }
 
+fn low_pass_comb_filter(audio: &mut AudioMaterial) {// TODO: implement
+    //https://www.dsprelated.com/freebooks/filters/Analysis_Digital_Comb_Filter.html
+    let g1: f32 = 0.5_f32.powf(3.0);
+    let g2: f32 = 0.9_f32.powf(5.0);
+    let mut backward_buffer: VecDeque<f32> = VecDeque::with_capacity(4);
+    backward_buffer.make_contiguous();
+    backward_buffer.extend([1.0, 0.0, 0.0, g1]);
+    let mut forward_buffer: VecDeque<f32> = VecDeque::with_capacity(4);
+    forward_buffer.make_contiguous();
+    forward_buffer.extend([1.0, 0.0, 0.0, 0.0, 0.0, g2]);
+
+    println!("backward_buffer: {:?}", backward_buffer);
+    println!("forward_buffer: {:?}", forward_buffer);
+
+    for block_nb in 0..audio.blocks.len() {
+        let block = audio.blocks[block_nb];
+        let mut new_block: [f32; BLOCK_SIZE] = [0.0; BLOCK_SIZE];
+
+        for n in 0..block.len() {
+            new_block[n] = block[n]; // TODO: implement
+
+            // "move" the buffers
+            backward_buffer.pop_front();
+            backward_buffer.push_back(block[n]);
+
+            forward_buffer.pop_front();
+            //forward_buffer.push_back(value); // Comment on fait, à la fin du block?
+        }
+        audio.blocks[block_nb] = new_block
+    }
+}
+
 fn MIFIR(audio: Wav<f32>) {
     //https://www.dsprelated.com/showarticle/1304.php // TODO LATER
     let fs = audio.sample_rate();
 }
 
+//
+// x_* hold the previous inputs
+// y_* hold the previous outputs
+// b* : the zeros (feedforward coefs)
+// a* : the poles (feedback coefs)
+// https://en.wikipedia.org/wiki/Digital_biquad_filter#Direct_form_1
+pub struct BiquadVars{
+    pub x_1:f32,
+    pub x_2:f32,
+    pub y_1:f32,
+    pub y_2:f32,
+    pub a1:f32,
+    pub a2:f32,
+    pub b0: f32, 
+    pub b1:f32,
+    pub b2:f32,
+}
+
+fn biquad_filter(audio: &mut AudioMaterial, vars: &mut BiquadVars) {
+    //https://www.dsprelated.com/freebooks/filters/Elementary_Filter_Sections.html
+    for block_nb in 0..audio.blocks.len() {
+        let block = audio.blocks[block_nb];
+        let mut new_block: [f32; BLOCK_SIZE] = [0.0; BLOCK_SIZE];
+
+        for n in 0..block.len() {
+            let mut result = vars.b0 * block[n];
+            result += vars.b1 * vars.x_1;
+            result += vars.b2 * vars.x_2;
+            result -= vars.a1 * vars.y_1;
+            result -= vars.a2 * vars.y_2;
+            // input sliding
+            vars.x_2 = vars.x_1;
+            vars.x_1 = block[n];
+            // output sliding
+            vars.y_2 = vars.y_1;
+            vars.y_1 = result;
+            new_block[n] = result
+        }
+        audio.blocks[block_nb] = new_block
+    }
+
+}
+
+
+
 #[cfg(test)]
 mod tests {
+    use std::f32::consts::PI;
+
+    use crate::utils::biquad_low_pass_params_generator;
+
     use super::*;
 
     #[test]
     fn test_naive_filter() {
-        let mut audio = AudioMaterial::new("tests/alto.wav");
+        let mut audio = AudioMaterial::new("tests/white-noise.wav");
         naive_low_pass(&mut audio);
-        audio.write("target/alto_lowpassed_naive.wav")
+        audio.write("target/white-noise_lowpassed_naive.wav")
     }
 
     #[test]
     fn test_single_pole_low_pass_filter() {
-        let mut audio = AudioMaterial::new("tests/alto.wav");
+        let mut audio = AudioMaterial::new("tests/white-noise.wav");
         single_pole_low_pass_filter(&mut audio, 50);
-        audio.write("target/alto_single_pole_low_pass_filter.wav")
+        audio.write("target/white-noise_single_pole_low_pass_filter.wav")
+    }
+    #[test]
+    fn test_low_pass_comb_filter() {
+        let mut audio = AudioMaterial::new("tests/white-noise.wav");
+        low_pass_comb_filter(&mut audio);
+        audio.write("target/white-noise_low_pass_comb_filter.wav")
+    }
+
+    #[test]
+    fn test_biquad_filter() {
+        let mut audio = AudioMaterial::new("tests/alto.wav");
+        let mut vars = biquad_low_pass_params_generator(audio.sample_rate, 500.0);
+        biquad_filter(&mut audio, &mut vars);
+        audio.write("target/alto_biquad_filter.wav")
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,32 @@
+use std::f32::consts::{LOG10_2, PI};
+
+use crate::filters::BiquadVars;
+
 pub const fn prev_power_of_two(n: u32) -> u32 {
     // n = 0 gives highest_bit_set_idx = 0.
     let highest_bit_set_idx = 31 - (n | 1).leading_zeros();
     // Binary AND of highest bit with n is a no-op, except zero gets wiped.
     (1 << highest_bit_set_idx) & n
+}
+
+pub fn biquad_low_pass_params_generator(fs: i32, cutoff_frequency: f32)-> BiquadVars{ 
+        let Q = 3.0;
+        let omega = (2.0*PI*cutoff_frequency)/fs as f32;
+        let alpha = omega.sin()*f32::sinh(LOG10_2/2.0*Q*omega/omega.sin());
+        
+        let a0 = 1.0+alpha;
+
+        let vars = BiquadVars{
+            x_1: 0.0,
+            x_2: 0.0, 
+            y_1: 0.0,
+            y_2: 0.0, 
+            //gain: 0.0809, 
+            a1: (-2.0*omega.cos()) / a0,
+            a2: (1.0 - alpha) / a0, 
+            b0: ((1.0 - omega.cos())/2.0) / a0,
+            b1: (1.0 - omega.cos()) / a0,
+            b2: ((1.0 - omega.cos())/2.0) / a0
+        };
+        vars
 }
